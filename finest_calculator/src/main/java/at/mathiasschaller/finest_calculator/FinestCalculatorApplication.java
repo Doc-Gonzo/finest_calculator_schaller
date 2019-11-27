03be6b24920021b9c1da99ffba65c8a927d7da6b
Doc-Gonzo <36294678+Doc-Gonzo@users.noreply.github.com>
2019-11-27 13:48:10 +0100

    Clean Start

---
+++ finest_calculator/src/main/java/at/mathiasschaller/finest_calculator/FinestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package at.mathiasschaller.finest_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinestCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinestCalculatorApplication.class, args);
+	}
+
+}
---
+++ finest_calculator/src/test/java/at/mathiasschaller/finest_calculator/FinestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mathiasschaller.finest_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinestCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
