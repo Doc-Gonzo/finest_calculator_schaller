0014d3abaf51b1fb06416e79b67612ac5b1c3b00
Mathias Schaller <stoertebecker@gmx.at>
2019-11-27 14:27:50 +0100

    Application Context fully repaired

--- finest_calculator/src/main/java/at/mathiasschaller/finest_calculator/FinestCalculatorApplication.java
+++ finest_calculator/src/main/java/at/mathiasschaller/finest_calculator/FinestCalculatorApplication.java
@@ -12,19 +12,18 @@ public class FinestCalculatorApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(FinestCalculatorApplication.class,args);
+		Calculator calculator = (Calculator) ctx.getBean("calculator");
+
 		Scanner scan = new Scanner(System.in);
 
-		Calculator calculator = new Calculator();
 		System.out.println("The World's Finest Calculator!");
 		System.out.println("Please enter number:");
-		double number1 = scan.nextDouble();
 
+		double number1 = scan.nextDouble();
 		System.out.println("Please enter another number:");
 		double number2 = scan.nextDouble();
 		double ergebnis = Calculator.calculateSumOfProduct(number1,number2);
 		System.out.println(" Summe dividiert durch das Produkt der beiden Zahlen: " + ergebnis);
-
-
 	}
 
 }
